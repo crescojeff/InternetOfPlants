b3c0f02a27565cd9d59827f301ed73c28d66969b
Jeff Creswell <creswel2@gmail.com>
2018-04-18 19:21:14 -0400

    init commit of boilerplate J2EE code against tomcat.  Spring MVC libs added

---
+++ InternetOfPlants/src/com/ai/iot/plants/controllers/SensorController.java
@@ -0,0 +1,15 @@
+package com.ai.iot.plants.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.ModelMap;
+
+@Controller
+@RequestMapping("/hello")
+public class SensorController {
+   @RequestMapping(method = RequestMethod.GET)public String printHello(ModelMap model) {
+      model.addAttribute("message", "Hello Spring MVC Framework!");
+      return "hello";
+   }
+}
